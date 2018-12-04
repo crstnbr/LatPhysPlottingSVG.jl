1b577d74aba86df041ff60eceb589534456c9d76
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-12-04 14:15:22 +0100

    Files generated by PkgTemplates

---
+++ src/LatPhysPlottingSVG.jl
@@ -0,0 +1,5 @@
+module LatPhysPlottingSVG
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LatPhysPlottingSVG
+using Test
+
+@testset "LatPhysPlottingSVG.jl" begin
+    # Write your own tests here.
+end
